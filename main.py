1b2377ecac801a74c5fa8e329cd04b9e23e20133
amr.cloudforce@protonmail.com <amr.cloudforce@protonmail.com>
2025-01-31 12:47:48 +0000

    wip

--- main.py
+++ main.py
@@ -5,7 +5,32 @@ doc = DocxTemplate("example.docx")
 
 # Create the context with our data
 context = {
-    'name': 'peter'
+    'name': 'Peter',
+    'jobs': [
+        {
+            'date_from': 'Mai 2012',
+            'date_to': 'Januar 2017',
+            'company': 'Musterladen GmbH',
+            'role': 'Tätigkeit als Projektmanager und Qualitätsverantwortlicher',
+            'tasks': [
+                'Einführung ISO 17020',
+                'Beschleunigung der Beschaffungsprozesse',
+                'Verbesserung der Kundenprozesse'
+            ]
+        },
+        {
+            # Second job entry
+            'date_from': 'Mai 2008',
+            'date_to': 'April 2012',
+            'company': 'SEOXPERT AG',
+            'role': 'Tätigkeit als Senior Projektmanager',
+            'tasks': [
+                'Grössere Kundenprojekte im Bereich eMarketing',
+                'Einführung div. Qualitätsmanagement-Systeme'
+            ]
+        }
+        # Add more jobs as needed
+    ]
 }
 
 # Render the template with our context
